c68995c1349b4b59c29f2110a6f16ab9274205b5
TruptiMusale <156177572+TruptiMusale@users.noreply.github.com>
2025-04-07 10:21:21 +0530

    Trupti test changes

--- AcornSourceCode/Ganesha/PIVF.BusinessLayer/FertivueDashboard/FertivueDashboardBAL.cs
+++ AcornSourceCode/Ganesha/PIVF.BusinessLayer/FertivueDashboard/FertivueDashboardBAL.cs
@@ -27,5 +27,26 @@ namespace PIVF.BusinessLayer.FertivueDashboard
         List<PatientList> GetFootFallDashboardPatientList(DateTime? FromDate, DateTime? ToDate, string Type = null);
         List<Datasetvo> GetAdminDashboardPercentageCounts(DateTime? FromDate, DateTime? ToDate, DateTime? LastFromDate, DateTime? LastToDate);
 
+
+        //git test
+
+        //Trends GetRegistrationDashboardByTrendsCount(DateTime? FromDate, DateTime? ToDate);
+        //Trends GetRegistrationDashboardByAgeGroupCount(DateTime? FromDate, DateTime? ToDate);
+        //Trends GetAppointmentDashboardCount(DateTime? FromDate, DateTime? ToDate, string AppType = null);
+        //ChartData GetInvestigationDashboardCount(DateTime? FromDate, DateTime? ToDate);
+        //BillingData GetBillingDashboardCount(DateTime? FromDate, DateTime? ToDate, string Breakdown = null);
+        //BillingData GetTotalRevenueDashboardServiceTypeCount(DateTime? FromDate, DateTime? ToDate);
+        //Trends GetToDoListDashboardCount(DateTime? FromDate, DateTime? ToDate);
+        //Trends GetFootFallDashboardCount(DateTime? FromDate, DateTime? ToDate);
+        //List<PatientList> GetRegistrationDashboardByTrendPatientList(DateTime? FromDate, DateTime? ToDate, string Type = null);
+        //List<PatientList> GetRegistrationDashboardByAgeGroupPatientList(DateTime? FromDate, DateTime? ToDate);
+        //List<PatientList> GetAppointmentDashboardPatientList(DateTime? FromDate, DateTime? ToDate, string AppType = null);
+        //List<PatientList> GetInvestigationDashboardPatientLiast(DateTime? FromDate, DateTime? ToDate, string Specialization = null);
+        //List<PatientList> GetBillingDashboardPatientList(DateTime? FromDate, DateTime? ToDate, string Breakdown = null);
+        //List<PatientList> GetTotalRevenueDashboardServiceTypePatientList(DateTime? FromDate, DateTime? ToDate, string Specialization = null);
+        //List<PatientList> GetToDoListDashboardPatientList(DateTime? FromDate, DateTime? ToDate, string Type = null);
+        //List<PatientList> GetFootFallDashboardPatientList(DateTime? FromDate, DateTime? ToDate, string Type = null);
+        //List<Datasetvo> GetAdminDashboardPercentageCounts(DateTime? FromDate, DateTime? ToDate, DateTime? LastFromDate, DateTime? LastToDate);
+
     }
 }
